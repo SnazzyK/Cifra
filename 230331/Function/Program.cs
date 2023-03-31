7837d5f52bad45897115672b827d4772c0b20c74
Valeriy <128506624+SnazzyK@users.noreply.github.com>
2023-03-31 20:49:27 +0200

    Function

---
+++ 230331/Function/Program.cs
@@ -0,0 +1,54 @@
+﻿
+int _seconds = 0;
+DateTime finishDateTime = new DateTime(2023, 3, 31, 23, 48, 0);
+DateTime _diffDateTime;
+
+while (true)
+{
+    
+    Console.Clear();
+    _seconds = AddSeconds(_seconds);
+    Console.WriteLine($"DayOfWeek:{GetDayOfWeek()}\nDay:{GetDay()}\nMonth:{GetMonth()}\nYear:{GetYear()}");
+    Console.WriteLine($"Time:{GetHours()}:{GetMinutes(DateTime.Now)}:{GetSeconds(DateTime.Now)}");
+    Console.WriteLine($"Second:{_seconds}");
+    _diffDateTime = new DateTime(finishDateTime.Ticks - DateTime.Now.Ticks);
+    Console.WriteLine($"До конца пары осталось: {GetMinutes(_diffDateTime)}:{GetSeconds(_diffDateTime)}");
+
+    Thread.Sleep(1000);
+}
+Console.ReadLine();
+//PROGRAM END
+
+string GetDayOfWeek()
+{
+    return DayOfWeek.Friday.ToString();
+}
+int GetHours()
+{
+    return DateTime.Now.Hour;
+}
+int GetDay()
+{
+    return DateTime.Now.Day;
+}
+int GetMonth()
+{
+    return DateTime.Now.Month;
+}
+int GetYear()
+{
+    return DateTime.Now.Year;
+}
+int AddSeconds(int seconds)
+{
+
+    return seconds + GetSeconds(DateTime.Now);
+}
+int GetSeconds(DateTime dateTimeParam)
+{
+    return dateTimeParam.Second;
+}
+int GetMinutes(DateTime dateTimeParam)
+{
+    return DateTime.Now.Minute;
+}
